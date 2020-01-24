15a5aad6513517dfec8a45a5f076522c3dc1f997
Priw8 <priwe8@gmail.com>
2020-01-24 23:13:53 +0100

    add a cooler hello

---
+++ hello.asm
@@ -0,0 +1,13 @@
+    global _main
+    extern _printf
+
+    section .data
+hello:
+    db "Hello world!",10,0
+
+    section .text
+_main:
+    push hello
+    call _printf
+    add esp, 4
+    ret
